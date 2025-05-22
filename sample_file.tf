f53a8b4cd3056982c6c87d3b0b9bea0ed00d6f49
Muhammad Sayyad Amin <97822048+Sayyad-Amin@users.noreply.github.com>
2025-05-22 18:35:06 +0500

    Create sample_file.tf

---
+++ sample_file.tf
@@ -0,0 +1,7 @@
+## Just creating sample file by single click using the power of terraform that is infra provision automation tool.
+
+resource local_file sample_file {
+
+        filename = "sample.txt"
+        content = "Just starting and revising terraform for automation"
+}
